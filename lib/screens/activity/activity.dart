8a99b446e5745416168771b914807bfd1415fffa
Pedro Azevedo <pedroazevedo141@gmail.com>
2023-03-27 22:01:54 -0300

    ADDED - tela que ira conter o calendario de eventos.

--- lib/screens/activity/activity.dart
+++ lib/screens/activity/activity.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:reu_app/constants.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 import '../../widgets/header_with_seachbox.dart';
 
@@ -11,6 +12,10 @@ class ActivityPage extends StatefulWidget {
 }
 
 class _ActivityPageState extends State<ActivityPage> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+
   static const TextStyle optionStyle =
       TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
   @override
@@ -21,13 +26,13 @@ class _ActivityPageState extends State<ActivityPage> {
       children: <Widget>[
         CustomScrollView(
           slivers: <Widget>[
-            SliverAppBar(
+            const SliverAppBar(
               floating: true,
               snap: true,
               backgroundColor: kPrimaryColor,
               elevation: 0.0,
               flexibleSpace: FlexibleSpaceBar(
-                title: const Text("Calendario de Atividades"),
+                title: Text("Calendario de Atividades"),
                 centerTitle: true,
               ),
             ),
@@ -35,7 +40,44 @@ class _ActivityPageState extends State<ActivityPage> {
               child: HeaderWithSearchBox(size: size),
             ),
             SliverToBoxAdapter(
-              child: const Padding(padding: EdgeInsets.all(400)),
+              child: TableCalendar(
+                firstDay: DateTime.utc(2010, 10, 16),
+                lastDay: DateTime.utc(2030, 3, 14),
+                focusedDay: _focusedDay,
+                calendarFormat: _calendarFormat,
+                selectedDayPredicate: (day) {
+                  // Use `selectedDayPredicate` to determine which day is currently selected.
+                  // If this returns true, then `day` will be marked as selected.
+
+                  // Using `isSameDay` is recommended to disregard
+                  // the time-part of compared DateTime objects.
+                  return isSameDay(_selectedDay, day);
+                },
+                onFormatChanged: (format) {
+                  if (_calendarFormat != format) {
+                    // Call `setState()` when updating calendar format
+                    setState(() {
+                      _calendarFormat = format;
+                    });
+                  }
+                },
+                onDaySelected: (selectedDay, focusedDay) {
+                  if (!isSameDay(_selectedDay, selectedDay)) {
+                    // Call `setState()` when updating the selected day
+                    setState(() {
+                      _selectedDay = selectedDay;
+                      _focusedDay = focusedDay;
+                    });
+                  }
+                },
+                onPageChanged: (focusedDay) {
+                  // No need to call `setState()` here
+                  _focusedDay = focusedDay;
+                },
+              ),
+            ),
+            const SliverToBoxAdapter(
+              child: Padding(padding: EdgeInsets.all(400)),
             )
           ],
         )
